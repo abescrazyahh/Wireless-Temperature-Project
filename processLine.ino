41a3eb72f3710b51ce2f8934768cc56b4edd9349
Suntana1 <suntana@hawaii.edu>
2017-02-21 12:14:05 -1000

    Add files via upload
    Added the ability to see the alarm value on display

---
+++ processLine.ino
@@ -0,0 +1,41 @@
+void processLine(char *s)
+{
+  int number = 0;
+  Serial.print(num++);
+  Serial.print(" ");
+  Serial.println(s);
+
+  char *f;
+  char buffer[100];
+  char *t = buffer;
+  strcpy(t, s);
+
+  Serial.print("* ");
+  Serial.println(t);
+
+  for (;;)
+  {
+    f = strsep(&t, ",");
+    if (f == NULL)
+    {
+      Serial.println("f==NULL");
+      break;
+    }
+    else
+    {
+      if (number == 0)
+      {
+        strcpy(temperature, f);
+      }
+      /* else if (number == 1)
+        {
+         strcpy(humidity, f);                     More sensors
+        }
+      */
+      //  Serial.print(number++);
+      //  Serial.print(" ");
+      //  Serial.println(f);
+    }
+
+  }
+}
